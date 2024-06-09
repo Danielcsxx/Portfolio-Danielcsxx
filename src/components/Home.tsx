53365a7ffc3bd985f011325c648bea44cbeab57f
Danielcsxx <Daniel.csxx@gmail.com>
2024-06-09 20:38:25 -0300

    Atualizacao do meu profile disponivel na aplicacao

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -239,7 +239,7 @@ const Home: React.FC = () => {
             <FaRegUser size={26} />
           </BtnContact>
           <BtnDownload
-            href="https://drive.google.com/file/d/1YefIwnzoQArtbAMVVLHvGC5xnDYmNCP_/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1V3TL424wcxQAyhCYq1DcpxNmS_xfJXsJ/view?usp=sharing"
             target="_blank"
           >
             {"Download"}
